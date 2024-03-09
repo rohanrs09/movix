3a96678bc2015e0c225c253c9f44cf2f31a5bcd4
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-03-09 12:43:15 +0530

    complete home page

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,27 @@ function App() {
     
   };
 
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
   return (
     <BrowserRouter>
-    {/* <Header/> */}
+    <Header/>
       <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/:mediaType/:id' element={<Details/>}/>
@@ -48,7 +66,7 @@ function App() {
       <Route path='/explore/:mediaType' element={<Explore />} />
       <Route path='*' element={<PageNotFound /> } />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 }
